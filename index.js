f1641bb06151e06bb9700c1cbdfb5fdca22787cb
Jose Aguilar <jaguilarny@gmail.com>
2022-04-28 17:25:01 -0400

    commit tree 93a5b06591303fbde4d566a450e969ed10372e3d

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+const superbowlWin = (record) => {
+    const winningGame = record.find(game => game.result === 'W');
+    if (winningGame) {
+        return winningGame.year
+    }
+    return undefined;
+}
